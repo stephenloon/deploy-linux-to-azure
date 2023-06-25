12115799dae5fb19174cd5bcb91590d558e0d93b
stephenloon <134666517+stephenloon@users.noreply.github.com>
2023-06-25 10:51:43 -0700

    Update install-lamp.sh

--- install-lamp.sh
+++ install-lamp.sh
@@ -11,26 +11,36 @@ sudo apt-get update
 # Functions to install LAMP
 
 install_apache() {
+if ! command -v apache2 &> /dev/null;
+then
+    echo "Apache is not installed. Installing..."
     sudo apt-get install apache2 -y
+    echo "Apache installation was successful!"
+else
+    echo "Apache is already installed!"
+fi
 }
 
 install_mysql() {
+if ! command -v mysql &> /dev/null;
+then
+    echo "MySQL is not installed. Installing..."
     sudo apt install mysql-server -y
-}
-
-install_mysql_secure() {
-    sudo mysql_secure_installation
-
-    # if you get this error " Failed! Error: SET PASSWORD has no significance for user 'root'@'localhost...."
-    # exit the set up
-    # enter "sudo mysql"
-    # enter this: ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password by 'new_password';
-    # enter desired password in 'new_password'
-    # follow the prompts after
+    echo "MySQL installation was successful!"
+else
+    echo "MySQL is already installed!"
+fi
 }
 
 install_php() {
+if ! command -v php &> /dev/null;
+then
+    echo "PHP is not installed. Installing..."
     sudo apt install php libapache2-mod-php -y
+    echo "PHP installation was successful!"
+else
+    echo "PHP is already installed!"
+fi
 
 }
 
@@ -45,19 +55,17 @@ echo "LAMP Stack Installation"
 
 while true;
 do
-    read -p "Select an option: [1] Apache [2] MySQL [3] MySQL Secure [4] PHP [5] Restart Apache [6] Quit: " choice
+    read -p "Select an option: [1] Apache [2] MySQL [3] PHP [4] Restart Apache [5] Quit: " choice
     case $choice in
         1) install_apache;;
 
         2) install_mysql;;
 
-        3) install_mysql_secure;;
+        3) install_php;;
 
-        4) install_php;;
+        4) restart_apache;;
 
-        5) restart_apache;;
-
-        6) echo "Quitting..."
+        5) echo "Quitting..."
             break;;
         *) echo "Invalid option. Please select something else";;
 
