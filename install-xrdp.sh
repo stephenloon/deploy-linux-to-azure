7afe648204e8a1af739cfb90bccab718d01fb7ca
stephenloon <134666517+stephenloon@users.noreply.github.com>
2023-06-25 16:20:24 -0700

    Update install-xrdp.sh

--- install-xrdp.sh
+++ install-xrdp.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 #Install xfce
-sudo apt-get update && sudo apt-get install xfce4-session
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install xfce4
+sudo apt install xfce4-session
 
 # Install and enable RDP server on Ubuntu
-sudo apt-get -y install xrdp && sudo systemctl enable xrdp
+sudo apt-get -y install xrdp
+sudo systemctl enable xrdp
 
-# Give certificate access to xrdp user
+# Give certificate access to xrdp user (Ubuntu 20 only)
 sudo adduser xrdp ssl-cert
 
 # Configure xrdp user use xfce as your desktop environment
-echo xfc4-session >~/.xsession
+echo xfce4-session >~/.xsession
 
 #restart xrdp service
 sudo service xrdp restart
+
 
 
